17d075926f08af5964c9aab998ded9e62fff5a09
Ghaithgaied98 <ghaithgaied10@gmail.com>
2021-03-28 19:39:08 +0100

    nodejscheckpoint

---
+++ makeitmodular.js
@@ -0,0 +1,28 @@
+var fs = require('fs')
+var path = require('path')
+
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+function getFiles(dir, filterStr, callback) {
+
+  fs.readdir(dir, function (err, list) {
+    if (err)
+      return callback(err)
+
+    list = list.filter(function (file) {
+      return path.extname(file) === '.' + filterStr
+    })
+
+    callback(null, list)
+  })
+}
+
+getFiles(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
